98c587eb85540d980509e8a32c3569dbc955f25f
MakiCode <trentonmaki@gmail.com>
2013-04-26 16:06:44 -0600

    Committing Collinear assignment

--- src/Brute.java
+++ src/Brute.java
@@ -1,15 +1,24 @@
 public class Brute {
   public static void main(String[] args) {
-    // we load the list points....
-    Point[] array = new Point[2];
-    for (Point p : array) {
-      for (Point q : array) {
-        for (Point r : array) {
-          for (Point s : array) {
+    In in = new In(args[0]);
+    int N = in.readInt();
+    Point[] a = new Point[N];
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (int i = 0; i < N; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      Point point = new Point(x, y);
+      point.draw();
+      a[i] = point;
+    }
+
+    for (Point p : a) {
+      for (Point q : a) {
+        for (Point r : a) {
+          for (Point s : a) {
             if (p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(r) == p.slopeTo(s)) {
               StdOut.println(p + " -> " + q + " -> " + r + " -> " + s);
-              // basic brute force implementation need to solve the p->q->r->s
-              // and p->q->s->r etc. problem
             }
           }
         }
--- src/Point.java
+++ src/Point.java
@@ -1,51 +1,53 @@
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
+  private int x;
+  private int y;
   public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() {
 
     @Override
-    public int compare(Point o1, Point o2) {
-      double slope1 = slopeTo(o1); //TODO still have to deal with degenrate cases
+    public int compare(final Point o1, final Point o2) {
+      if (o1 == null || o2 == null) {
+        throw new NullPointerException();
+      }
+      double slope1 = slopeTo(o1);
       double slope2 = slopeTo(o2);
-      if(slope1 < slope2) {
+      if (slope1 < slope2) {
         return -1;
-      } else if(slope1 > slope2) {
+      } else if (slope1 > slope2) {
         return 1;
       } else {
         return 0;
       }
     }
   };        // compare points by slope to this point
-  private int x;
-  private int y;
 
-   public Point(int x, int y){
-     this.x = x;
-     this.y = y;
-   }// construct the point (x, y)
+   public Point(int x1, int y1){
+     x = x1;
+     y = y1;
+   }
 
    public   void draw(){
      StdDraw.point(x, y);
    }
    public   void drawTo(Point that){
      StdDraw.line(x, y, that.x, that.y);
-   }// draw the line segment from this point to that point
+   }
 
    public String toString() {
      return "(" + x + ", " + y + ")";
    }
-  
 
   public int compareTo(Point that) {
-    if(that == null) {
+    if (that == null) {
       throw new NullPointerException();
     }
-    if(y < that.y) {
+    if (y < that.y) {
       return -1;
-    } else if(y == that.y) {
-      if(x < that.x) {
-        return -1; 
-      } else if(x > that.x) {
+    } else if (y == that.y) {
+      if (x < that.x) {
+        return -1;
+      } else if (x > that.x) {
         return 1;
       } else {
         return 0;
@@ -53,19 +55,19 @@ public class Point implements Comparable<Point> {
     } else {
       return 1;
     }
-  }// is this point lexicographically smaller than that point?
+  }
   public double slopeTo(Point that) {
     double yDiff = that.y - y;
     double xDiff = that.x - x;
-    if(xDiff == 0 && yDiff == 0) {
+    if (xDiff == 0 && yDiff == 0) {
       return Double.NEGATIVE_INFINITY;
     }
-    if(yDiff == 0) {
+    if (yDiff == 0) {
      return +0;
-    } else if(xDiff == 0) {
+    } else if (xDiff == 0) {
       return Double.POSITIVE_INFINITY;
     } else {
-      return yDiff/xDiff;
+      return yDiff / xDiff;
     }
   } // the slope between this point and that point
 }
