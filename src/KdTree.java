dd86971a6c21f974eaebf11d95046c047fcba8c5
Trenton Maki <trentonmaki@iMac-215.local>
2013-05-10 12:01:36 -0600

    Added A KDTree test. Tests are not rigorous and require work

--- src/KdTree.java
+++ src/KdTree.java
@@ -2,176 +2,219 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KdTree {
-  private Node root;
-  private int size;
+	private Node root;
+	private int size;
 
-  public KdTree() {
-  } // construct an empty set of points
+	public KdTree() {
+	} // construct an empty set of points
 
-  private class Node {
-    private Node left;
-    private Node right;
-    private Point2D value;
+	private class Node {
+		private Node left;
+		private Node right;
+		private Point2D value;
 
-    public Node(Point2D val) {
-      this.value = val;
-      this.left = null;
-      this.right = null;
-    }
+		public Node(Point2D val) {
+			this.value = val;
+			this.left = null;
+			this.right = null;
+		}
 
-    @Override
-    public String toString() {
-      return "{" + left + "} " + value + " {" + right + "}";
+		@Override
+		public String toString() {
+			return "{" + left + "} " + value + " {" + right + "}";
 
-    }
-  }
+		}
+	}
 
-  public void printTree() {
-    System.out.println(root);
-  }
+	public void printTree() {
+		System.out.println(root);
+	}
 
-  public boolean isEmpty() {
-    return root == null;
-  } // is the set empty?
+	public boolean isEmpty() {
+		return root == null;
+	} // is the set empty?
 
-  public int size() {
-    return size;
-  } // number of points in the set
+	public int size() {
+		return size;
+	} // number of points in the set
 
-  public void insert(Point2D p) {
-    root = insert(root, p, 0);
-  } // add the point p to the set (if it is not already in the set)
+	public void insert(Point2D p) {
+		root = insert(root, p, 0);
+	} // add the point p to the set (if it is not already in the set)
 
-  private Node insert(Node node, Point2D p, int height) {
-    if (node == null) {
-      size++;
-      Node node2 = new Node(p);
-      return node2;
-    }
-    if (node.value.equals(p)) {
-      return node;
-    }
-    if (height % 2 == 0) {
-      if (p.x() < node.value.x()) {
-        node.left = insert(node.left, p, height + 1);
-        return node;
-      } else {
-        node.right = insert(node.right, p, height + 1);
-        return node;
-      }
-    } else {
-      if (p.y() < node.value.y()) {
-        node.left = insert(node.left, p, height + 1);
-        return node;
-      } else {
-        node.right = insert(node.right, p, height + 1);
-        return node;
-      }
-    }
-  }
+	private Node insert(Node node, Point2D p, int height) {
+		if (node == null) {
+			size++;
+			Node node2 = new Node(p);
+			return node2;
+		}
+		if (node.value.equals(p)) {
+			return node;
+		}
+		if (height % 2 == 0) {
+			if (p.x() < node.value.x()) {
+				node.left = insert(node.left, p, height + 1);
+				return node;
+			} else {
+				node.right = insert(node.right, p, height + 1);
+				return node;
+			}
+		} else {
+			if (p.y() < node.value.y()) {
+				node.left = insert(node.left, p, height + 1);
+				return node;
+			} else {
+				node.right = insert(node.right, p, height + 1);
+				return node;
+			}
+		}
+	}
 
-  public boolean contains(Point2D p) {
-    return contains(root, p, 0);
-  } // does the set contain the point p?
+	public boolean contains(Point2D p) {
+		return contains(root, p, 0);
+	} // does the set contain the point p?
 
-  private boolean contains(Node node, Point2D p, int height) {
-    if (node == null) {
-      return false;
-    }
-    if (node.value.equals(p)) {
-      return true;
-    }
-    if (height % 2 == 0) {
-      if (p.x() < node.value.x()) {
-        return contains(node.left, p, height + 1);
-      } else {
-        return contains(node.right, p, height + 1);
-      }
-    } else {
-      if (p.y() < node.value.y()) {
+	private boolean contains(Node node, Point2D p, int height) {
+		if (node == null) {
+			return false;
+		}
+		if (node.value.equals(p)) {
+			return true;
+		}
+		if (height % 2 == 0) {
+			if (p.x() < node.value.x()) {
+				return contains(node.left, p, height + 1);
+			} else {
+				return contains(node.right, p, height + 1);
+			}
+		} else {
+			if (p.y() < node.value.y()) {
 
-        return contains(node.left, p, height + 1);
-      } else {
-        return contains(node.right, p, height + 1);
-      }
-    }
-  }
+				return contains(node.left, p, height + 1);
+			} else {
+				return contains(node.right, p, height + 1);
+			}
+		}
+	}
 
-  public void draw() {
-    draw(root, root.value.x(), root.value.y(), 0);
-  } // draw all of the points to standard draw
+	public void draw() {
+		draw(root, root.value.x(), root.value.y(), 0);
+	} // draw all of the points to standard draw
 
-  private void draw(Node node, double xEdge, double yEdge, int height) {
-    if (node == null) {
-      return;
-    }
-    StdDraw.setPenRadius(.01);
-    StdDraw.setPenColor(StdDraw.BLACK);
-    StdDraw.point(node.value.x(), node.value.y());
-    draw(node.left, 0, 0, height+1);
-    draw(node.right, 0, 0, height+1);
-    if (height % 2 == 0) {
-      StdDraw.setPenColor(StdDraw.RED);
-      StdDraw.setPenRadius();
-      StdDraw.line(xEdge, node.value.y(), 1, node.value.y());
-      draw(node.left, node.value.x(), node.value.y(), height + 1);
-      draw(node.right, node.value.x(), node.value.y(), height + 1);
-    } else {
-      StdDraw.setPenColor(StdDraw.BLUE);
-      StdDraw.setPenRadius();
-      StdDraw.line(node.value.x(), yEdge, node.value.x(), 1);
-      draw(node.left, node.value.x(), node.value.y(), height + 1);
-      draw(node.right, node.value.x(), node.value.y(), height + 1);
-    }
-  }
+	private void draw(Node node, double xEdge, double yEdge, int height) {
+		if (node == null) {
+			return;
+		}
+		StdDraw.setPenRadius(.01);
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.point(node.value.x(), node.value.y());
+		draw(node.left, 0, 0, height + 1);
+		draw(node.right, 0, 0, height + 1);
+		if (height % 2 == 0) {
+			StdDraw.setPenColor(StdDraw.RED);
+			StdDraw.setPenRadius();
+			StdDraw.line(xEdge, node.value.y(), 1, node.value.y());
+			draw(node.left, node.value.x(), node.value.y(), height + 1);
+			draw(node.right, node.value.x(), node.value.y(), height + 1);
+		} else {
+			StdDraw.setPenColor(StdDraw.BLUE);
+			StdDraw.setPenRadius();
+			StdDraw.line(node.value.x(), yEdge, node.value.x(), 1);
+			draw(node.left, node.value.x(), node.value.y(), height + 1);
+			draw(node.right, node.value.x(), node.value.y(), height + 1);
+		}
+	}
 
-  public Iterable<Point2D> range(RectHV rect) {
-    List<Point2D> points = new ArrayList<Point2D>();
-    range(root, rect, 0, points);
-    return points;
-  } // all points in the set that are inside the rectangle
+	public Iterable<Point2D> range(RectHV rect) {
+		List<Point2D> points = new ArrayList<Point2D>();
+		range(root, rect, 0, points);
+		return points;
+	} // all points in the set that are inside the rectangle
 
-  private void range(Node node, RectHV r, int height, List<Point2D> points) {
-    if (node == null) {
-      return;
-    }
-    if (r.contains(node.value)) {
-      points.add(node.value);
-      range(node.left, r, height + 1, points);
-      range(node.right, r, height + 1, points);
-    } else {
-      if (height % 2 == 0) {
-        if (r.ymin() < node.value.y() && r.ymax() > node.value.y()) {
-          range(node.left, r, height + 1, points);
-          range(node.right, r, height + 1, points);
-        } else {
-          if (r.ymin() > node.value.y()) {
-            range(node.right, r, height + 1, points);
-          }
-          if (r.ymax() < root.value.y()) {
-            range(node.left, r, height + 1, points);
-          }
-        }
-      } else {
-        if (r.xmin() < node.value.x() && r.xmax() > node.value.x()) {
-          range(node.left, r, height + 1, points);
-          range(node.right, r, height + 1, points);
-        } else {
-          if (r.xmin() > node.value.x()) {
-            range(node.right, r, height + 1, points);
-          }
-          if (r.xmax() < node.value.x()) {
-            range(node.left, r, height + 1, points);
-          }
-        }
-      }
-    }
-    // TODO Auto-generated method stub
+	private void range(Node node, RectHV r, int height, List<Point2D> points) {
+		if (node == null) {
+			return;
+		}
+		if (r.contains(node.value)) {
+			points.add(node.value);
+			range(node.left, r, height + 1, points);
+			range(node.right, r, height + 1, points);
+		} else {
+			if (height % 2 == 0) {
+				if (r.ymin() < node.value.y() && r.ymax() > node.value.y()) {
+					range(node.left, r, height + 1, points);
+					range(node.right, r, height + 1, points);
+				} else {
+					if (r.ymin() > node.value.y()) {
+						range(node.right, r, height + 1, points);
+					}
+					if (r.ymax() < root.value.y()) {
+						range(node.left, r, height + 1, points);
+					}
+				}
+			} else {
+				if (r.xmin() < node.value.x() && r.xmax() > node.value.x()) {
+					range(node.left, r, height + 1, points);
+					range(node.right, r, height + 1, points);
+				} else {
+					if (r.xmin() > node.value.x()) {
+						range(node.right, r, height + 1, points);
+					}
+					if (r.xmax() < node.value.x()) {
+						range(node.left, r, height + 1, points);
+					}
+				}
+			}
+		}
+		// TODO Auto-generated method stub
 
-  }
+	}
 
-  public Point2D nearest(Point2D p) {
-    return null;
-  } // a nearest neighbor in the set to p; null if set is empty
+	public Point2D nearest(Point2D p) {
+		if (contains(p)) {
+			return p;
+		}
+		return nearest(root, p, root.value.distanceSquaredTo(p), 0);
+	} // a nearest neighbor in the set to p; null if set is empty
+
+	private Point2D nearest(Node node, Point2D point, double bestDistance,
+			int height) {
+		if (node == null) {
+			return null;
+		} 
+		if(node.left == null && node.right == null) {
+			return node.value;
+		}
+		double distance = node.value.distanceSquaredTo(point);
+		if (height % 2 == 0) {
+			if (point.x() >= node.value.x()) {
+				if (distance < bestDistance) {
+					return nearest(node.right, point, distance, height + 1);
+				} else {
+					return nearest(node.right, point, bestDistance, height + 1);
+				}
+			} else {
+				if (distance < bestDistance) {
+					return nearest(node.left, point, distance, height + 1);
+				} else {
+					return nearest(node.left, point, bestDistance, height + 1);
+				}
+			}
+		} else {
+			if (point.y() >= node.value.y()) {
+				if (distance < bestDistance) {
+					return nearest(node.right, point, distance, height + 1);
+				} else {
+					return nearest(node.right, point, bestDistance, height + 1);
+				}
+			} else {
+				if (distance < bestDistance) {
+					return nearest(node.left, point, distance, height + 1);
+				} else {
+					return nearest(node.left, point, bestDistance, height + 1);
+				}
+			}
+		}
+	}
 
 }
--- src/KdTreeTest.java
+++ src/KdTreeTest.java
@@ -1,7 +1,6 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 
 public class KdTreeTest {
 
@@ -14,9 +13,8 @@ public class KdTreeTest {
 		boolean test2 = kdTree.size() == 1;
 		assertTrue("Expected was: true result was: " + test, test);
 		assertTrue("Expected was: true result was: " + test2, test2);
-		//kdTree.printTree();
 	}
-	
+
 	@Test
 	public void testInsertMultiple() {
 		KdTree kdTree = new KdTree();
@@ -44,7 +42,106 @@ public class KdTreeTest {
 		assertTrue(kdTree.contains(point6));
 		assertTrue(kdTree.contains(point7));
 		assertTrue(kdTree.contains(point8));
-		kdTree.printTree();
+	}
+
+	@Test
+	public void testMultipleSameInsert() {
+		KdTree kdTree = new KdTree();
+		Point2D point1 = new Point2D(50, 50);
+		Point2D point2 = new Point2D(50, 50);
+		kdTree.insert(point2);
+		kdTree.insert(point2);
+		kdTree.insert(point1);
+		kdTree.insert(point2);
+		kdTree.insert(point1);
+		kdTree.insert(point2);
+		kdTree.insert(point2);
+		kdTree.insert(point1);
+		kdTree.printTree(); // Don't know how to test this with junit since I
+							// need to know internals of tree
+	}
+
+	@Test
+	public void testRange() {
+		KdTree kdTree = new KdTree();
+		RectHV rect = new RectHV(0, 0, 100, 100);
+		Point2D point = new Point2D(50, 50);
+		kdTree.insert(point);
+		Iterable<Point2D> pointsInRange = kdTree.range(rect);
+		assertTrue(pointsInRange.iterator().hasNext());
+		Point2D point2d = pointsInRange.iterator().next();
+		assertTrue(point2d.equals(point));
+	}
+
+	@Test
+	public void testMultipleRange() {
+		KdTree kdTree = new KdTree();
+		RectHV rectHV = new RectHV(0, 0, 100, 100);
+		Point2D point1 = new Point2D(50, 50);
+		Point2D point2 = new Point2D(70, 70);
+		Point2D point3 = new Point2D(30, 30);
+		kdTree.insert(point1);
+		kdTree.insert(point2);
+		kdTree.insert(point3);
+		Iterable<Point2D> pointsInRange = kdTree.range(rectHV);
+		for (Point2D point : pointsInRange) {
+			assertTrue(point.equals(point1) || point.equals(point2)
+					|| point.equals(point3));
+		}
+	}
+
+	@Test
+	public void testMultipleInOutRange() {
+		KdTree kdTree = new KdTree();
+		RectHV rectHV = new RectHV(39, 39, 100, 100);
+		Point2D point1 = new Point2D(50, 50);
+		Point2D point2 = new Point2D(70, 70);
+		Point2D point3 = new Point2D(40, 40);
+		Point2D point4 = new Point2D(30, 30);
+		Point2D point5 = new Point2D(20, 20);
+		Point2D point6 = new Point2D(200, 200);
+		kdTree.insert(point1);
+		kdTree.insert(point2);
+		kdTree.insert(point3);
+		kdTree.insert(point4);
+		kdTree.insert(point5);
+		kdTree.insert(point6);
+		Iterable<Point2D> pointsInRange = kdTree.range(rectHV);
+		for (Point2D point : pointsInRange) {
+			assertTrue(!point.equals(point4) && !point.equals(point5)
+					&& !point.equals(point6));
+			assertTrue(point.equals(point1) || point.equals(point2)
+					|| point.equals(point3));
+		}
+	}
+
+	@Test
+	public void testOutOfRange() {
+		KdTree kdTree = new KdTree();
+		RectHV rect = new RectHV(0, 0, 1, 1);
+		Point2D point1 = new Point2D(30, 30);
+		Point2D point2 = new Point2D(40, 40);
+		Point2D point3 = new Point2D(50, 50);
+		kdTree.insert(point1);
+		kdTree.insert(point2);
+		kdTree.insert(point3);
+		Iterable<Point2D> pointsInRange = kdTree.range(rect);
+		assertTrue(!pointsInRange.iterator().hasNext());
+	}
+	
+	@Test
+	public void emptyContains() {
+		KdTree kdTree = new KdTree();
+		assertTrue(!kdTree.contains(new Point2D(0, 0)));
+	}
+	
+	@Test
+	public void testNearest() {
+		KdTree kdTree = new KdTree();
+		Point2D point = new Point2D(4, 7);
+		kdTree.insert(point);
+		Point2D queryPoint = new Point2D(6,  90);
+		assertTrue(kdTree.nearest(queryPoint).equals(point));
 	}
 
 }
